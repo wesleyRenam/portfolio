bcc95e3851801793bc6cada7e71937834980fa8b
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-24 19:41:54 -0300

    aumentando ele

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import cvIcon from '../img/cv.svg'
 function Home() {
   return (
     <div className='flex flex-col w-screen h-screen justify-center items-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
-        <div className=''>
+        <div className='transform scale-150'>
           <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white text-smp font-semibold font-roboto text-white font-sans ml-2">PRAZER, ME CHAMO WESLEY E SOU </h1>
           <div className='flex items-center'>
             <h1 className="font-bold text-9xl text-white">REACT </h1>
