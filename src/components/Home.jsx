6b50e65f6067f47829da3a4273543dd5df33e82d
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-24 11:04:34 -0300

    colocando o nome

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,8 @@ function Home() {
     <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
       <div className='flex w-screen h-screen justify-center items-center'>
         <div>
-          <div className='flex items-center '>
+          <h1 className="text-sm font-bold text-white font-sans">PRAZER, ME CHAMO WESLEY E SOU </h1>
+          <div className='flex items-center mt-0'>
             <h1 className="font-bold text-9xl text-white">REACT </h1>
             <a href="https://www.linkedin.com/in/wesleyrenam/" target="_blank"  rel="noreferrer">
               <img src={linkedinIcon} alt='linkedinIcon' className='w-20 ml-4 mt-5 hover:scale-125 ease-in-out duration-300'/>
