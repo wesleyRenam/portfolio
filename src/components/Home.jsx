f2aea2d55f076849a9f14e92c4d5565ae50a6638
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-24 19:40:25 -0300

    add animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,10 @@ import cvIcon from '../img/cv.svg'
 
 function Home() {
   return (
-    <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
-      <div className='flex w-screen h-screen justify-center items-center'>
-        <div>
-          <h1 className="text-sm font-semibold font-roboto text-white font-sans">PRAZER, ME CHAMO WESLEY E SOU </h1>
-          <div className='flex items-center mt-0'>
+    <div className='flex flex-col w-screen h-screen justify-center items-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
+        <div className=''>
+          <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white text-smp font-semibold font-roboto text-white font-sans ml-2">PRAZER, ME CHAMO WESLEY E SOU </h1>
+          <div className='flex items-center'>
             <h1 className="font-bold text-9xl text-white">REACT </h1>
             <a href="https://www.linkedin.com/in/wesleyrenam/" target="_blank"  rel="noreferrer">
               <img src={linkedinIcon} alt='linkedinIcon' className='w-20 ml-4 mt-5 hover:scale-125 ease-in-out duration-300'/>
@@ -22,9 +21,11 @@ function Home() {
             </a>
           </div>
           <h1 className="font-bold text-9xl text-white">DEVELOPER </h1>
+          <div id='descricao' className='ml-auto flex mt-2 w-96 '>
+          <h1 className="font-bold text-end text-sm text-white">TENHO 21 ANOS, CURSANDO CIÊNCIA DA COMPUTAÇÃO E COM 1 ANO DE EXPERIÊNCIA COM PROJETOS PESSOAIS NA TRYBE. </h1>
+        </div>
         </div>
       </div>
-    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,30 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['Roboto Slab', ...defaultTheme.fontFamily.sans],
-        'roboto': ['Roboto'],
+        'roboto': ['Josefin'],
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "40%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 5s steps(50) infinite alternate, blink .7s infinite"
+      }
     },
   },
   plugins: [],
