511f4d76ee52f15de73f96fbe300411d1bd6183b
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-24 10:46:33 -0300

    background e hover

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,22 @@ import cvIcon from '../img/cv.svg'
 
 function Home() {
   return (
-    <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500'>
+    <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
       <div className='flex w-screen h-screen justify-center items-center'>
         <div>
           <div className='flex items-center '>
-            <h1 className="font-bold text-9xl">REACT </h1>
+            <h1 className="font-bold text-9xl text-white">REACT </h1>
             <a href="https://www.linkedin.com/in/wesleyrenam/" target="_blank"  rel="noreferrer">
-              <img src={linkedinIcon} alt='linkedinIcon' className='w-20 ml-4 mt-5'/>
+              <img src={linkedinIcon} alt='linkedinIcon' className='w-20 ml-4 mt-5 hover:scale-125 ease-in-out duration-300'/>
             </a>
             <a href="https://github.com/wesleyRenam" target="_blank"  rel="noreferrer">
-              <img src={githubIcon} alt='githubIcon' className='w-20 ml-4 mt-5'/>
+              <img src={githubIcon} alt='githubIcon' className='w-20 ml-4 mt-5 hover:scale-125 ease-in-out duration-300'/>
             </a>
             <a href={require('../cv.pdf')} target="_blank"  rel="noreferrer">
-              <img src={cvIcon} alt='cvIcon' className='w-16 ml-4 mt-5'/>
+              <img src={cvIcon} alt='cvIcon' className='w-16 ml-4 mt-5 hover:scale-125 ease-in-out duration-300'/>
             </a>
           </div>
-          <h1 className="font-bold text-9xl">DEVELOPER </h1>
+          <h1 className="font-bold text-9xl text-white">DEVELOPER </h1>
         </div>
       </div>
     </div>
