eccace92b0e78ad72713e437784e2354be515c9d
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-26 15:35:32 -0300

    arrumando o responsivo

--- src/components/Project2.jsx
+++ src/components/Project2.jsx
@@ -5,9 +5,9 @@ import githubIcon from '../img/square-github.svg';
 
 function Projects() {
   return(
-    <div className='Pastel bg-gradient-to-tr from-violet-500 to-orange-300'>
-      <div className='animate-text grid grid-cols-1 place-items-center w-auto h-screen p-4 lg:grid-cols-2'>
-        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='lg:scale-[1.5]'>
+    <div className='Pastel bg-gradient-to-tr from-violet-500 to-orange-300 '>
+      <div className='animate-text grid grid-cols-1 place-items-center w-auto h-screen p-4 lg:grid-cols-2 '>
+        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='2xl:scale-[2.0] xl:scale-[1.6] lg:scale-[1.3]'>
           <div className='max-w-sm tracking-tight shadow-3xl rounded-xl p-4'>
             <img src={ghibli} alt="" className='rounded-xl drop-shadow-2xl' />
             <div className='flex items-end'>
@@ -20,21 +20,7 @@ function Projects() {
           </div>
         </a>
         
-        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='lg:scale-[1.5]'>
-          <div className='max-w-sm shadow-3xl tracking-tight p-4 rounded-xl'>
-            <img src={ghibli2} alt="" className='rounded-xl drop-shadow-2xl mt-0' />
-            <div className='flex items-end'>
-              <h1 className='text-5xl mt-4 text-left font-bold text-white'>GHIBLI LIST PROJECT</h1>
-              <a href="https://github.com/wesleyRenam" target="_blank"  rel="noreferrer">
-                <img src={githubIcon} alt='githubIcon' className='w-12 mb-3 mr-1 lg:w-20  lg:mt-5 md:mt-5 hover:scale-125 ease-in-out duration-300'/>
-              </a>
-            </div>
-            <h3 className='text-left text-gray-100 text-sm'>PROJETO PARA MOSTRAR O MEU AMOR PELO STUDIO GHIBLI</h3>
-          </div>
-        </a>
-      </div>
-      <div className='animate-text grid grid-cols-1 place-items-center w-auto h-screen p-4 lg:grid-cols-2'>
-        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='lg:scale-[1.5]'>
+        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='2xl:scale-[2.0] xl:scale-[1.6] lg:scale-[1.3]'>
           <div className='max-w-sm tracking-tight shadow-3xl rounded-xl p-4'>
             <img src={ghibli} alt="" className='rounded-xl drop-shadow-2xl' />
             <div className='flex items-end'>
@@ -46,9 +32,24 @@ function Projects() {
             <h3 className='text-left text-gray-100 text-sm'>PROJETO PARA MOSTRAR O MEU AMOR PELO STUDIO GHIBLI</h3>
           </div>
         </a>
-        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='lg:scale-[1.5]'>
-          <div className='max-w-sm shadow-3xl tracking-tight p-4 rounded-xl'>
-            <img src={ghibli2} alt="" className='rounded-xl drop-shadow-2xl mt-0' />
+      </div>
+      <div className='animate-text grid grid-cols-1 place-items-center w-auto h-screen p-4 lg:grid-cols-2 min-[375px]:mt-5'>
+      <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='2xl:scale-[2.0] xl:scale-[1.6] lg:scale-[1.3]'>
+          <div className='max-w-sm tracking-tight shadow-3xl rounded-xl p-4'>
+            <img src={ghibli} alt="" className='rounded-xl drop-shadow-2xl' />
+            <div className='flex items-end'>
+              <h1 className='text-5xl mt-4 text-left font-bold text-white'>GHIBLI LIST PROJECT</h1>
+              <a href="https://github.com/wesleyRenam" target="_blank"  rel="noreferrer">
+                <img src={githubIcon} alt='githubIcon' className='w-12 mb-3 mr-1 lg:w-20  lg:mt-5 md:mt-5 hover:scale-125 ease-in-out duration-300'/>
+              </a>
+            </div>
+            <h3 className='text-left text-gray-100 text-sm'>PROJETO PARA MOSTRAR O MEU AMOR PELO STUDIO GHIBLI</h3>
+          </div>
+        </a>
+
+        <a href="https://ghibli-project-9eg6ovxt5-wesleyrenam.vercel.app/home" target="_blank"  rel="noreferrer" className='2xl:scale-[2.0] xl:scale-[1.6] lg:scale-[1.3]'>
+          <div className='max-w-sm tracking-tight shadow-3xl rounded-xl p-4'>
+            <img src={ghibli} alt="" className='rounded-xl drop-shadow-2xl' />
             <div className='flex items-end'>
               <h1 className='text-5xl mt-4 text-left font-bold text-white'>GHIBLI LIST PROJECT</h1>
               <a href="https://github.com/wesleyRenam" target="_blank"  rel="noreferrer">
@@ -59,7 +60,7 @@ function Projects() {
           </div>
         </a>
       </div>
-      
+      <div className='hidden h-16 min-[375px]:block'></div>
     </div>     
   )
 }
