ab77ad51c6cb744c94b19e38cbadc8b9a3618db9
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-26 16:07:36 -0300

    Começando a fazer a section about me

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './components/Home'
 import Mobile from './components/Mobile'
 import Projects from './components/Project2'
+import About from './components/About'
 
 function App() {
   return (
     <div>
       <Mobile />
       <Projects />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return(
+    <div className='h-screen w-auto animate-text Pastel bg-gradient-to-tr from-violet-500 to-orange-300'>
+
+    </div>
+  )
+
+}
+
+export default About;
