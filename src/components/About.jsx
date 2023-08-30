7bb2c8cbe03e3cd3ea40e30cb91c783b7f0d0f39
wesleyRenam <wesleyrenamjob@gmail.com>
2023-08-29 22:04:08 -0300

    Webp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import perfil from '../img/perfil.jpg'
+import perfil from '../img/perfil-1.webp'
 import css from '../icons/css3.svg'
 import html from '../icons/html5.svg'
 import js from '../icons/javascript.svg'
@@ -16,13 +16,13 @@ function About() {
   return(
     <div className='h-screen w-auto animate-text Pastel bg-gradient-to-tr from-violet-500 to-orange-300'>  
       <div className='animate-text grid grid-cols-1 place-items-center w-auto h-screen p-4 lg:grid-cols-1'>
-        <div className='w-full h-screen grid place-items-center m-0 lg:grid-cols-2'>
+        <div className='w-full h-screen grid place-items-center m-0 lg:grid-cols-2 p-0'>
           <div className=''>
             <p className='text-base text-center text-teal-200 mb-2 font-montserrat'>Sobre Mim</p>
             <h1 className='text-4xl text-center font-bold text-white font-lato'>ME CHAMO <span className='text-teal-200'>WESLEY RENAM</span></h1>
             {/* <p className='text-base text-center text-teal-200 font-montserrat'>21 Anos</p> */}
             <h2 className='text-xl text-center text-white my-2 font-lato'>FULLSTACK REACT E NODEJS DEVELOPER</h2>
-            <p className='text-sm text-center font-montserrat text-white'>Graduando em ciência da computação e formando em fullstack developer pela trybe, contendo 1 ano de experiência com desenvolvimento de softwares responsivos, escaláveis e com alto desempenho</p>
+            <p className='text-sm text-center font-montserrat text-white'>Graduando em ciência da computação e formando em fullstack developer pela trybe, contendo 1 ano de experiência com desenvolvimento de softwares responsivos, escaláveis e com alto desempenho tenho um prazer enorme em resolver problemas e aprender novas tecnologias.</p>
             <img src={perfil} alt='Minha foto de perfil' className='border mt-4 shadow-xl rounded-xl drop-shadow-2xl'/>
           </div>          
         </div>
